4fd40d567e33ddaeca5c9053b8ed9334d65124e2
Brian Moore <brian.moore@cloudruler.io>
2021-03-18 20:15:49 -0500

    Allow everything

--- main.tf
+++ main.tf
@@ -44,97 +44,123 @@ resource "azurerm_application_security_group" "asg_k8s_workers" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-# resource "azurerm_network_security_group" "nsg_main" {
-#   name                = "nsg-main"
-#   location            = var.location
-#   resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_network_security_group" "nsg_main" {
+  name                = "nsg-main"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-#   #Allow SSH inbound
-#   security_rule {
-#     name                       = "nsg-allow-ssh-snet-${local.landing_zone_name}"
-#     description                = "Allow Inbound SSH"
-#     priority                   = 1001
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_range     = "22"
-#     source_address_prefix      = "*"
-#     destination_address_prefix = "VirtualNetwork"
-#   }
+  security_rule {
+    name                                       = "allow-in-all"
+    description                                = "Allow Inbound All"
+    priority                                   = 100
+    direction                                  = "Inbound"
+    protocol                                   = "*"
+    source_address_prefix                      = "*"
+    source_port_range                          = "*"
+    destination_address_prefix                 = "*"
+    destination_port_range                     = "*"
+    access                                     = "Allow"
+  }
 
-#   #k8s master/worker node rules
-#   security_rule {
-#     name                                       = "allow-in-kubelet-api"
-#     description                                = "Allow Inbound to kubelet API (used by self, control plane)"
-#     priority                                   = 1002
-#     direction                                  = "Inbound"
-#     protocol                                   = "Tcp"
-#     source_address_prefix                      = "*"
-#     source_port_range                          = "*"
-#     destination_application_security_group_ids = [azurerm_application_security_group.asg_k8s_masters.id, azurerm_application_security_group.asg_k8s_workers.id]
-#     destination_port_range                     = "10250"
-#     access                                     = "Allow"
-#   }
+  security_rule {
+    name                                       = "allow-out-all"
+    description                                = "Allow Outbound All"
+    priority                                   = 101
+    direction                                  = "Outbound"
+    protocol                                   = "*"
+    source_address_prefix                      = "*"
+    source_port_range                          = "*"
+    destination_address_prefix                 = "*"
+    destination_port_range                     = "*"
+    access                                     = "Allow"
+  }
 
-#   security_rule {
-#     name                                       = "allow-in-kube-scheduler"
-#     description                                = "Allow Inbound to kube-scheduler (used by self)"
-#     priority                                   = 1003
-#     direction                                  = "Inbound"
-#     protocol                                   = "Tcp"
-#     source_address_prefix                      = "*"
-#     source_port_range                          = "*"
-#     destination_application_security_group_ids = [azurerm_application_security_group.asg_k8s_masters.id]
-#     destination_port_range                     = "10251"
-#     access                                     = "Allow"
-#   }
+  #Allow SSH inbound
+  security_rule {
+    name                       = "nsg-allow-ssh-snet-${local.landing_zone_name}"
+    description                = "Allow Inbound SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "VirtualNetwork"
+  }
 
-#   #k8s master
-#   security_rule {
-#     name                                       = "allow-in-k8s-api"
-#     description                                = "Allow Inbound to Kubernetes API server"
-#     priority                                   = 1004
-#     direction                                  = "Inbound"
-#     protocol                                   = "Tcp"
-#     source_address_prefix                      = "*"
-#     source_port_range                          = "*"
-#     destination_application_security_group_ids = [azurerm_application_security_group.asg_k8s_masters.id]
-#     destination_port_range                     = "6443"
-#     access                                     = "Allow"
-#   }
+  #k8s master/worker node rules
+  security_rule {
+    name                                       = "allow-in-kubelet-api"
+    description                                = "Allow Inbound to kubelet API (used by self, control plane)"
+    priority                                   = 1002
+    direction                                  = "Inbound"
+    protocol                                   = "Tcp"
+    source_address_prefix                      = "*"
+    source_port_range                          = "*"
+    destination_application_security_group_ids = [azurerm_application_security_group.asg_k8s_masters.id, azurerm_application_security_group.asg_k8s_workers.id]
+    destination_port_range                     = "10250"
+    access                                     = "Allow"
+  }
 
-#   security_rule {
-#     name                                       = "allow-in-etcd-clientapi"
-#     description                                = "Allow Inbound to etcd server client API (used by kube-apiserver, etcd)"
-#     priority                                   = 1005
-#     direction                                  = "Inbound"
-#     protocol                                   = "Tcp"
-#     source_address_prefix                      = "*"
-#     source_port_range                          = "*"
-#     destination_application_security_group_ids = [azurerm_application_security_group.asg_k8s_masters.id]
-#     destination_port_range                     = "2379-2380"
-#     access                                     = "Allow"
-#   }
+  security_rule {
+    name                                       = "allow-in-kube-scheduler"
+    description                                = "Allow Inbound to kube-scheduler (used by self)"
+    priority                                   = 1003
+    direction                                  = "Inbound"
+    protocol                                   = "Tcp"
+    source_address_prefix                      = "*"
+    source_port_range                          = "*"
+    destination_application_security_group_ids = [azurerm_application_security_group.asg_k8s_masters.id]
+    destination_port_range                     = "10251"
+    access                                     = "Allow"
+  }
 
-#   security_rule {
-#     name                                       = "allow-in-kube-controller-manager"
-#     description                                = "Allow Inbound to kube-controller-manager (used by self)"
-#     priority                                   = 1006
-#     direction                                  = "Inbound"
-#     protocol                                   = "Tcp"
-#     source_address_prefix                      = "*"
-#     source_port_range                          = "*"
-#     destination_application_security_group_ids = [azurerm_application_security_group.asg_k8s_masters.id]
-#     destination_port_range                     = "10252"
-#     access                                     = "Allow"
-#   }
-# }
+  #k8s master
+  security_rule {
+    name                                       = "allow-in-k8s-api"
+    description                                = "Allow Inbound to Kubernetes API server"
+    priority                                   = 1004
+    direction                                  = "Inbound"
+    protocol                                   = "Tcp"
+    source_address_prefix                      = "*"
+    source_port_range                          = "*"
+    destination_application_security_group_ids = [azurerm_application_security_group.asg_k8s_masters.id]
+    destination_port_range                     = "6443"
+    access                                     = "Allow"
+  }
 
-# resource "azurerm_subnet_network_security_group_association" "nsg_snet_main" {
-#   subnet_id                 = azurerm_subnet.snet_main.id
-#   network_security_group_id = azurerm_network_security_group.nsg_main.id
-# }
+  security_rule {
+    name                                       = "allow-in-etcd-clientapi"
+    description                                = "Allow Inbound to etcd server client API (used by kube-apiserver, etcd)"
+    priority                                   = 1005
+    direction                                  = "Inbound"
+    protocol                                   = "Tcp"
+    source_address_prefix                      = "*"
+    source_port_range                          = "*"
+    destination_application_security_group_ids = [azurerm_application_security_group.asg_k8s_masters.id]
+    destination_port_range                     = "2379-2380"
+    access                                     = "Allow"
+  }
+
+  security_rule {
+    name                                       = "allow-in-kube-controller-manager"
+    description                                = "Allow Inbound to kube-controller-manager (used by self)"
+    priority                                   = 1006
+    direction                                  = "Inbound"
+    protocol                                   = "Tcp"
+    source_address_prefix                      = "*"
+    source_port_range                          = "*"
+    destination_application_security_group_ids = [azurerm_application_security_group.asg_k8s_masters.id]
+    destination_port_range                     = "10252"
+    access                                     = "Allow"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_snet_main" {
+  subnet_id                 = azurerm_subnet.snet_main.id
+  network_security_group_id = azurerm_network_security_group.nsg_main.id
+}
 
 locals {
   admin_username = "cloudruleradmin"

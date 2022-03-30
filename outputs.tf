1b2d66cf792f8b1c02830eb32115b6075eabeb59
Brian Moore <brianmoore@cloudruler.com>
2022-03-30 21:29:29 +0000

    Removing outputs

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "master_custom_data" {
-  value     = module.aks_cluster.master_custom_data
-  sensitive = true
-}
-
-output "worker_custom_data" {
-  value     = module.aks_cluster.worker_custom_data
-  sensitive = true
-}
